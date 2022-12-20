834273afbc663ced4e3584692ff926161a7d4fbf
Connor McDermott <cmcdermt@amazon.com>
2022-12-20 10:59:42 -0600

    Small turret changes

--- Scripts/Buildings/Turrets/Turret.cs
+++ Scripts/Buildings/Turrets/Turret.cs
@@ -3,19 +3,26 @@ using System;
 
 public class Turret : KinematicBody
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+
+	private Spatial head;
+	private Spatial rotator;
 
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	public override void _Ready() {
+		this.head = GetNode<Spatial>("Body/Rotator/Head");
+		this.rotator = GetNode<Spatial>("Body/Rotator");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta) {
 		
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	private Node findTarget() {
+		IntersectShape(new PhysicsShapeQueryParameters());
+	}
+
+	private void turretLook(Vector3 target) {
+
+	}
 }
